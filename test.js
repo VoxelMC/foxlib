b0133119e58dcddcdf55920ac4ca6287584c3b67
Voxel <voxelmc@hotmail.com>
2019-09-13 21:14:06 -0700

    Update test.js

--- test.js
+++ test.js
@@ -43,6 +43,9 @@ const fci = new FConsoleInput({
             }
             
             console.log(output);
+        },
+        sub: (n1, n2) => {
+            console.log(n1 - n2);
         }
     },
     exit: { 
